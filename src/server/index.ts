8207b8ffaaf7295182ac5d0e4b2573cce8912ae2
D4rkr34lm <darth.m.frohn@gmail.com>
2025-12-19 15:54:55 +0100

    Fix barrel file

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 export { createApiEndpointHandler } from "./handlers/api/createApiHandler";
-export { buildAuthenticationMiddleware } from "./middleware/auth";
+export { createBasicAuthSchema } from "./security/basicAuth";
+export { createBearerAuthSchema } from "./security/bearerAuth";
 export { createServer, type Server, type ServerConfig } from "./server";
 
 export default {};
